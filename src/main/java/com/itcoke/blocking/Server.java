4968b7266d0e2c864cb374df212d47fc4296c43e
yushuai <yushuai@shimo.im>
2022-01-20 08:34:43 +0800

    fix

--- src/main/java/com/itcoke/blocking/Server.java
+++ src/main/java/com/itcoke/blocking/Server.java
@@ -27,44 +27,22 @@ public class Server {
 
         // 3、连接集合
         List<SocketChannel> channels = new ArrayList<>();
-        log.debug("12345");
         while(true){
             // 4、accept 建立与客户端的链接
             log.info("建立连接。。。。");
             SocketChannel accept = ssc.accept();
-            log.info("链接成功。。。。");
+            log.info("连接成功。。。。",accept);
             channels.add(accept);
             // 5、接收客户端的数据
             for(SocketChannel sc : channels){
-                log.info("读取数据开始");
+                log.info("读取数据开始",sc);
                 sc.read(buffer);
                 buffer.flip();
                 ByteBufferUtil.debugRead(buffer);
                 buffer.clear();
-                log.info("读取数据结束");
+                log.info("读取数据结束",sc);
             }
         }
 
-    }
-
-    public static void readBufferByChannel(SocketChannel channel) throws IOException {
-        ByteBuffer buffer = ByteBuffer.allocate(16);
-        // 从 channel 读取数据填充 ByteBuffer，返回值表示读到了多少字节，-1 表示到达了文件的末尾
-        int read = channel.read(buffer);
-        while (read != -1) {
-            System.out.println(channel.getLocalAddress()+"读取的字节数为：" + read);
-            // 切换到读模式
-            buffer.flip();
-            // 打印读取到 buffer 中的内容
-            while (buffer.hasRemaining()) {
-                System.out.print((char) buffer.get());
-            }
-            System.out.println();
-            // 重置到写模式
-            buffer.clear();
-            read = channel.read(buffer);
-            System.out.println(111);
-        }
-        log.info("读取结束endend");
     }
 }
